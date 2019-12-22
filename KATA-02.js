bba61f4167a385d3684b5946ecc40c42cde46846
Maryery Vargas <maryeryvargas@gmail.com>
2019-12-22 13:15:24 +0000

    first part basic arabic to roman

--- KATA-02.js
+++ KATA-02.js
@@ -187,3 +187,42 @@ let validator = roman => {
     return true;
 }
 console.log(validator('MCMXCIX'));
+
+/* Crear una función para pasar de árabes a romanos*/
+
+let arabicToRoman = number => {
+    switch (number) {
+        case 1:
+            return 'I';
+        case 2:
+            return 'II';
+        case 3:
+            return 'III';
+        case 4:
+            return 'IV';
+        case 5:
+            return 'V';
+        case 6:
+            return 'VI';
+        case 7:
+            return 'VII';
+        case 8:
+            return 'VIII';
+        case 9:
+            return 'IX';
+        case 10:
+            return 'X';
+        case 50:
+            return 'L';
+        case 100:
+            return 'C';
+        case 500:
+            return 'D';
+        case 1000:
+            return 'M';
+        default:
+            console.log('Enter a number 1,5,6,7,8,9,10,50,100,500,1000');
+
+    }
+}
+console.log(arabicToRoman(10));
